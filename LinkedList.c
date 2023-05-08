aedb61ff086600032d095cd6a3317b98e4f9a8e2
LeeJinHyoung <122239418+LeeJinHyoung@users.noreply.github.com>
2023-05-08 19:11:18 +0900

    refactor: Add dummy head to existing linked list and implement stack and queue

--- LinkedList.c
+++ LinkedList.c
@@ -8,23 +8,88 @@ typedef struct NODE
     struct NODE* next;
 }NODE;
 
-NODE* g_pHead = NULL;
+NODE* g_head = NULL;
 
-void PrintList(void)
+int IsEmpty()
 {
-    NODE* pHead = g_pHead;
-
-    while (pHead != NULL)
+    if (g_head->next == NULL)
+        return 1;
+    return 0;
+}
+void PrintList()
+{
+    while (g_head != NULL)
     {
-        printf("[%p] %s, next[%p]\n", pHead, pHead->szData, pHead->next);
-        pHead = pHead->next;
+        printf("[%p] %s, next[%p]\n", g_head, g_head->szData, g_head->next);
+
+        g_head = g_head->next;
     }
-    putchar('\n');
+}
+int InsertAtHead(char* pszData)
+{
+    NODE* pNode = (NODE*)malloc(sizeof(NODE));
+    memset(pNode, 0, sizeof(NODE));
+
+    strcpy_s(pNode->szData, sizeof(pNode->szData), pszData); //pNode의 Data에 pszData를 넣어준다.
+
+    if (IsEmpty())
+        g_head = pNode;
+    else {
+        pNode->next = g_head;
+        g_head = pNode;
+    }
+
+    return 1;
+}
+
+int InsertAtTail(char* pszData)
+{
+    NODE* pTmp = &g_head;
+    while (pTmp->next != 0)
+        pTmp = pTmp->next;
+
+    NODE* pNode = (NODE*)malloc(sizeof(NODE));
+    memset(pNode, 0, sizeof(NODE));
+
+    strcpy_s(pNode->szData, sizeof(pNode->szData), pszData);
+
+    pTmp->next = pNode;
+}
+
+
+NODE* FindData(char* pszData)
+{
+    NODE* pCur = g_head->next;
+    NODE* pPrev = &g_head;
+    while (pCur != NULL)
+    {
+        if (strcmp(pCur->szData, pszData) == 0)
+            return pPrev;
+        pCur = pCur->next;
+        pPrev = pPrev->next;
+    }
+
+    return 0;
+}
+
+int DeleteData(char* pszData)
+{
+    NODE* pPrev = FindData(pszData);
+    if (pPrev != 0)
+    {
+        NODE* pDelete = pPrev->next;
+        pPrev->next = pDelete->next;
+
+        printf("DeleteData(): %s\n", pDelete->szData);
+        free(pDelete);
+        return 1;
+    }
 }
 
 void ReleaseList(void)
 {
-    NODE* pTmp = g_pHead;
+    printf("\nReleaseList()");
+    NODE* pTmp = g_head->next;
     while (pTmp != NULL)
     {
         NODE* pDelete = pTmp;
@@ -32,80 +97,59 @@ void ReleaseList(void)
         printf("Delete: [%p] %s\n", pDelete, pDelete->szData);
         free(pDelete);
     }
+    g_head->next = 0;
 }
 
-int FindData(char* pszData)
+void PushData(char* pszData)
 {
-    NODE* pTmp = g_pHead;
-    while (pTmp != NULL)
-    {
-        if (strcmp(pTmp->szData, pszData) == 0)
-            return 1;
-        pTmp = pTmp->next;
-    }
+    InsertAtHead(pszData);
 }
 
-int DeleteData(char* pszData)
-{
-    NODE* pTmp = g_pHead;
-    NODE* pPrev = NULL;
-    while (pTmp != NULL)
-    {
-        if (strcmp(pTmp->szData, pszData) == 0)
-        {
-            printf("DeleteData(): %s\n", pTmp->szData);
-            if (pPrev != NULL)
-                pPrev->next = pTmp->next;
-            else {
-                g_pHead = pTmp->next;
-            }
-            free(pTmp);
-            return 1;
-        }
-        pPrev = pTmp;
-        pTmp = pTmp->next;
-    }
-}
+int PopData(NODE* pPopNode) {
+    NODE* sp = g_head->next;
+    if (IsEmpty())
+        return 0;
 
-int InsertNewNode(char* pszData)
-{
-    NODE* pNode = (NODE*)malloc(sizeof(NODE));
-    memset(pNode, 0, sizeof(NODE));
+    memcpy(pPopNode, sp, sizeof(NODE));
 
-    strcpy_s(pNode->szData, sizeof(pNode->szData), pszData);
-
-    if (g_pHead == NULL)
-        g_pHead = pNode;
-    else {
-        pNode->next = g_pHead;
-        g_pHead = pNode;
-    }
+    g_head->next = sp->next;
+    free(sp);
 
     return 1;
 }
 
+int EnQueue(char* pszData)
+{
+    return InsertAtTail(pszData);
+}
+
+int Dequeue(NODE* pGetNode)
+{
+    return PopData(pGetNode);
+}
+
 int main()
 {
-    InsertNewNode("TEST01");
+    EnQueue("TEST01");
+    EnQueue("TEST02");
+    EnQueue("TEST03");
+
     PrintList();
-    InsertNewNode("TEST02");
+
+    NODE node = { 0 };
+    Dequeue(&node);
+    printf("Dequeq: %s\n", node.szData);
     PrintList();
-    InsertNewNode("TEST03");
+
+    Dequeue(&node);
+    printf("Dequeq: %s\n", node.szData);
     PrintList();
 
-    if (FindData("TEST01") == 1)
-        printf("FindData(): Test01 found\n");
-
-    if (FindData("TEST02") == 1)
-        printf("FindData(): Test02 found\n");
-
-    if (FindData("TEST03") == 1)
-        printf("FindData(): Test03 found\n");
-
-    DeleteData("Test01");
-    DeleteData("Test02");
-    DeleteData("Test03");
+    Dequeue(&node);
+    printf("Dequeq: %s\n", node.szData);
+    PrintList();
 
     ReleaseList();
+
     return 0;
 }
